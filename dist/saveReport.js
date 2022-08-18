2bccdb0573d2af1b4d14e5c912d540dec9485f2c
borilla <github@borilla.co.uk>
2022-08-18 21:45:01 +1000

    Prevent saveReport from causing errors in tests
    - Use promise based version of fs
    - Prevents jest from warning of writing to logs after tests have completed

--- dist/saveReport.js
+++ dist/saveReport.js
@@ -5,16 +5,16 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = saveReport;
 
-var fs = require('fs');
+var fs = require('fs/promises');
 
 function saveReport(global) {
   var report = global.reduxReport.generate();
   if (!fs || !report) throw new Error('Could not save redux use report');
   var reportTypes = ['used', 'unused'];
-  reportTypes.forEach(function (descriptor) {
+  return Promise.all(reportTypes.map(function (descriptor) {
     fs.writeFile("./redux_report--".concat(descriptor, ".json"), JSON.stringify(report[descriptor]), function (err) {
       if (err) throw err;
       console.log("The ".concat(descriptor, " redux report has been saved!"));
     });
-  });
+  }));
 }
--- src/saveReport.js
+++ src/saveReport.js
@@ -1,14 +1,15 @@
-const fs = require('fs')
+const fs = require('fs/promises')
 
-export default function saveReport (global) {
+export default function saveReport(global) {
   const report = global.reduxReport.generate()
   if (!fs || !report) throw new Error('Could not save redux use report')
 
-  const reportTypes = ['used', 'unused']
-  reportTypes.forEach(descriptor => {
+  const reportTypes = ['used', 'unused'];
+
+  return Promise.all(reportTypes.map(descriptor => {
     fs.writeFile(`./redux_report--${descriptor}.json`, JSON.stringify(report[descriptor]), err => {
       if (err) throw err
       console.log(`The ${descriptor} redux report has been saved!`)
     })
-  })
+  }))
 }
